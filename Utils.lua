a01108c04a42989a73dee4a3e75f2ff6a3880a64
S3R43o3 <67480273+sera619@users.noreply.github.com>
2025-01-17 20:18:30 +0100

    create Utils file

---
+++ Utils.lua
@@ -0,0 +1,10 @@
+KeyAnnouncerUtils = {}
+
+
+function KeyAnnouncerUtils.PrintRed(text)
+    return "|cffff0000"..text.."|r"
+end
+
+function KeyAnnouncerUtils.PrintGreen(text)
+    return "|cff00ff00"..text.."|r"
+end
